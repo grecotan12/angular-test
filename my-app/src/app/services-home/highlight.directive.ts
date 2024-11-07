06236b490ee415229012fe16b85892975ac6f9ec
tan nguyen <bivani2805@gmail.com>
2024-11-07 22:26:42 +0000

    Learn about directives

--- my-app/src/app/services-home/highlight.directive.ts
+++ my-app/src/app/services-home/highlight.directive.ts
@@ -1,14 +1,35 @@
-import { Directive, ElementRef } from '@angular/core';
+// import { Directive, ElementRef } from '@angular/core';
 
+// @Directive({
+//   selector: '[appHighlight]',
+//   standalone: true
+// })
+// export class HighlightDirective {
+
+//   constructor(el: ElementRef) { 
+//     debugger;
+//     el.nativeElement.style.backgroundColor = 'purple'; 
+//  }
+// }
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 @Directive({
-  selector: '[appHighlight]',
-  standalone: true
+  standalone: true,
+  selector: '[appHighlight]'
 })
+
 export class HighlightDirective {
+  @Input() appHighlight = ''
+    
+  constructor(private el: ElementRef) {}
 
-  constructor(el: ElementRef) { 
-    debugger;
-    el.nativeElement.style.backgroundColor = 'purple'; 
- }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('green');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
 
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
--- my-app/src/app/services-home/services-home.component.ts
+++ my-app/src/app/services-home/services-home.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+
 @Component({
   selector: 'app-services-home',
   standalone: true,
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, NgFor, NgIf, NgSwitch, NgSwitchDefault],
   templateUrl: './services-home.component.html',
   styleUrl: './services-home.component.css'
 })
-export class ServicesHomeComponent {
 
+export class ServicesHomeComponent implements OnInit {
+  // bankingServices = ['Issue business loans',
+  //  'Open new checking and/or saving accounts',
+  //  'Help clients with account-related issues such as transfers, withdrawals, and deposits',
+  //  'Provide periodic reports and analysis',
+  // 'Monitor client accounts and keep information up to date',
+  // 'Guide clients through their savings, loan, mortgage, and retirement options',
+  //  'Refer customers to loan officers or other financial specialists',
+  // 'Resolve debit and credit issues'];
+  // bankingServices: any[] = [
+  //   {
+  //     "type": "Issue business loans",
+  //     "years": 2
+  //   },
+  //   {
+  //     "type": "Provide periodic reports and analysis",
+  //     "years": 7
+  //   },
+  //   {
+  //     "type": "Open new checking and/or saving accounts",
+  //     "years": 5
+  //   },
+  //   {
+  //     "type": "Monitor client accounts and keep information up-to-date",
+  //     "years": 3
+  //   },
+  //   {
+  //     "type": "Resolve debit and credit issues",
+  //     "years": 2
+  //   }
+  // ];
+
+  color = '';
+  bankingServices: any[] = [
+    {
+      "type": 'Issue business loans',
+      "years": 2,
+      "category": "loans"
+    },    {
+      "type": 'Provide periodic reports and analysis',
+      "years": 7,
+      "category": "reports"
+    },    {
+      "type": 'Open new checking and/or saving accounts',
+      "years": 5,
+      "category": "financial_accounts"
+    },    {
+      "type": 'Guide clients through their savings, loan, mortgage, and retirement options',
+      "years": 6,
+      "category":"options"
+    }
+  ];
+  ngOnInit(): void { }
 }
